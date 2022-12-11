1e1a0cb69d9e243fc4d68627710a6ee86366900e
Muhammadnosir <erkinovmuhammadnosir57@gmail.com>
2022-12-11 11:47:23 +0500

    sxa

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var salom = alert('xush kelibsz')
+var joy = alert('joylashuvga ruxsat berin?')
+var rasm = alert('galareyaga ruxsat berin?')
+var ism = prompt(' ismingizni yozin')
+var fam = prompt(' familiyangizni yozin')
+var yosh = prompt(' yoshingizni yozin')
+
+console.log('ism');
+console.log('fam');
+console.log('yoshj');
